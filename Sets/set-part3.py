009aa09f7d6ef691621c08d286756706a6175431
vinit12321 <vinitdabake800@gmail.com>
2020-09-28 00:22:14 +0530

    Set functions like Pop Remove and Discard

---
+++ Sets/set-part3.py
@@ -0,0 +1,19 @@
+n = int(input())
+s = set(map(int, input().split()))
+m=int(input())
+for i in range(m):
+    cmd=input().split()
+   
+    print(cmd[0])
+    if cmd[0]=="pop":
+        
+        s.pop()
+    if cmd[0]=="remove":
+        s.remove(int(cmd[1]))
+        
+    if cmd[0]=="discard":
+        s.discard(int(cmd[1]))
+        
+
+print(sum(s))
+
