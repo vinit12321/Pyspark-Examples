655afc8f7520288b647dc27835accef525f1979d
vinit12321 <vinitdabake800@gmail.com>
2020-09-28 00:43:40 +0530

    added symetric differnece

--- Sets/set-part4.py
+++ Sets/set-part4.py
@@ -3,6 +3,7 @@ seta= set(input().split())
 b= int(input())
 setb= set(input().split())
 
-print(len(seta.intersection(setb)))
-print(len(seta.union(setb)))
-print(len(seta.difference(setb)))
+print(len(seta.intersection(setb))) # comman betwen two set
+print(len(seta.union(setb))) # union unique betwen two set 
+print(len(seta.difference(setb))) # present in first set but not is second set
+print(len(seta.symmetric_difference(setb))) #All elements excepts command elements form both set
