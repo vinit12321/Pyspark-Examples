73a8da4e5bcc48ba7fefe3e97262d08c95404850
vinit12321 <vinitdabake800@gmail.com>
2020-11-13 23:19:31 +0530

    String Folder added

---
+++ Strings/string_count_overlapp.py
@@ -0,0 +1,30 @@
+def CountOccurrences(string, substring): 
+
+	# Initialize count and start to 0 
+	count = 0
+	start = 0
+
+	# Search through the string till 
+	# we reach the end of it 
+	while start < len(string): 
+
+		# Check if a substring is present from 
+		# 'start' position till the end 
+		pos = string.find(substring, start) 
+
+		if pos != -1: 
+			# If a substring is present, move 'start' to 
+			# the next position from start of the substring 
+			start = pos + 1
+
+			# Increment the count 
+			count += 1
+		else: 
+			# If no further substring is present 
+			break
+	# return the value of count 
+	return count 
+
+# Driver Code 
+string = "BANANA"
+print(CountOccurrences(string, "ANA")) 
