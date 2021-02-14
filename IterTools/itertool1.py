dd926859f93cfff611ecfc5ef2c45849f58b0d1a
vinit12321 <vinitdabake800@gmail.com>
2021-02-15 00:19:39 +0530

    Permuations function in itertools

---
+++ IterTools/itertool1.py
@@ -0,0 +1,10 @@
+from  itertools import permutations
+str,per=input().split()
+lis=sorted(''.join(i) for i in permutations(str,int(per)))
+for k in lis:
+    print(k)
+
+# SMART SOLUTIONS
+s="HACK"
+n=2
+print(*[''.join(i) for i in permutations(sorted(s),int(n))],sep='\n')
