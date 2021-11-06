c712593fa186a498a485efc3d0e13818061f4021
vinit12321 <vinitdabake800@gmail.com>
2021-11-06 19:45:31 +0530

    counter problem code

---
+++ Collection/counter_problem.py
@@ -0,0 +1,15 @@
+from collections import Counter
+no_of_shoes=int(input())
+shoes_size=list(map(int,input().split()))
+print(shoes_size)
+no_of_cust=int(input())
+shoes=Counter(shoes_size)
+total=0
+for _ in range(no_of_cust):
+    quant,price=map(int,input().split())
+    if shoes[quant]>0:
+        shoes[quant]-=1
+        total=total+price
+
+print(total)
+
